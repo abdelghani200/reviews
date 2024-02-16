ed6e89a0bebe8d584494f370bd178068378709c7
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-02-16 16:11:58 +0100

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContenuComponent } from './components/contenu/contenu.component';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { RegisterComponent } from './components/Auth/register/register.component';
+import { ReviewComponent } from './components/review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,component: ContenuComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path: 'reviews', component: ReviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
